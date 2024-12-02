a91d13a8af1ce0ccd0e6451e59d06cd2c76bcbf4
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2024-12-02 13:12:32 -0300

    Update app module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,7 +1,36 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './modules/user/user.module';
+import { UserSchema } from './modules/user/infrastructure/persistence/user.schema';
+import { AuthModule } from './modules/auth/auth.module';
+import { TaskModule } from './modules/task/task.module';
+
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: parseInt(configService.get('DB_PORT')),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [UserSchema],
+        synchronize: true,
+      }),
+    }),
+    UserModule,
+    AuthModule,
+    TaskModule,
+  ],
   controllers: [],
   providers: [],
 })
