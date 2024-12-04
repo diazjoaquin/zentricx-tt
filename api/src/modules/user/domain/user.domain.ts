c4115a31281e1ac679b39e296e71c1746b94e505
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2024-12-04 17:39:58 -0300

    Update user domain

--- api/src/modules/user/domain/user.domain.ts
+++ api/src/modules/user/domain/user.domain.ts
@@ -1,9 +1,7 @@
 import { Base } from "src/common/domain/base.domain";
-import { Role } from "./role.enum";
 
 export class User extends Base {
   name: string;
   email: string;
   password: string;
-  role: Role;
 }
