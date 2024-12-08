9ecb16354b82fa731d72af5eecd7236ace1cbf77
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2024-12-08 12:37:08 -0300

    Add api service

---
+++ ui/src/app/services/api.service.ts
@@ -0,0 +1,86 @@
+import { FetchConfig } from "../config/fetch/fetch.config";
+import { IApiService } from "../interfaces/IApiService";
+import { IFetchConfig } from "../interfaces/IFetchConfig";
+import { getCookie } from "../utils/cookies/cookies.utils";
+
+
+class ApiService implements IApiService {
+	private config: IFetchConfig;
+
+  constructor(config: IFetchConfig = FetchConfig) {
+		this.config = config;
+	}
+
+  async get<T = unknown>(endpoint: string): Promise<T> {
+		const response = await fetch(`${this.config.baseUrl}/${endpoint}`, {
+			method: 'GET',
+			headers: this.config.headers,
+		});
+		const responseJSON = await response.json();
+
+		if (!response.ok) {
+			throw new Error(responseJSON.message);
+		}
+
+		return responseJSON;
+	}
+
+	async post<T = unknown, K = unknown>(endpoint: string, body: K): Promise<T> {
+		const response = await fetch(`${this.config.baseUrl}/${endpoint}`, {
+			method: 'POST',
+			headers: this.config.headers,
+			body: JSON.stringify(body),
+		});
+		const responseJSON = await response.json();
+
+		if (!response.ok) {
+			throw new Error(responseJSON.message);
+		}
+
+		return responseJSON;
+	}
+
+	async patch<T = unknown, K = unknown>(endpoint: string, body: K): Promise<T> {
+		try {
+			const response = await fetch(`${this.config.baseUrl}/${endpoint}`, {
+				method: 'PATCH',
+				headers: this.config.headers,
+				body: JSON.stringify(body),
+			});
+			return response.json();
+		} catch (error) {
+			throw error as Error;
+		}
+	}
+
+	async put<T = unknown, K = unknown>(endpoint: string, body: K): Promise<T> {
+		try {
+			const token = await getCookie('access_token');
+			const response = await fetch(`${this.config.baseUrl}/${endpoint}`, {
+				method: 'PUT',
+				headers: {
+					...this.config.headers,
+					access_token: token ? token : '',
+				},
+				body: JSON.stringify(body),
+			});
+			return response.json();
+		} catch (error) {
+			throw error as Error;
+		}
+	}
+
+	async delete<T = unknown>(endpoint: string): Promise<T> {
+		try {
+			const response = await fetch(`${this.config.baseUrl}/${endpoint}`, {
+				method: 'DELETE',
+				headers: this.config.headers,
+			});
+			return response.json();
+		} catch (error) {
+			throw error as Error;
+		}
+	}
+}
+
+export default new ApiService();
