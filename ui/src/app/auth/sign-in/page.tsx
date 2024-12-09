c176558d2ff6f6224bf550bea689e86ae36d8d08
Joaquín Díaz <diaz.joaquin.e@gmail.com>
2024-12-09 03:51:49 -0300

    Add conditional auth rendering to sign-in and sign-up pages

--- ui/src/app/auth/sign-in/page.tsx
+++ ui/src/app/auth/sign-in/page.tsx
@@ -1,13 +1,16 @@
 'use client';
 import { SignInForm } from "@/app/components/Sign-in";
 import { useAuth } from "@/app/hooks/useAuth";
+import { useAuthContext } from "@/app/hooks/useAuthContext";
 import apiService from "@/app/services/api.service";
+import { redirect } from "next/navigation";
 
 
 const SignIn = () => {
+	const { authState } = useAuthContext();
 	const { handleSignIn, initialValuesSignIn } = useAuth();
 
-	return (
+	return authState.isAuth ? redirect('/') : (
 		<SignInForm 
 			apiService={apiService} 
 			handleSignIn={handleSignIn} 
--- ui/src/app/auth/sign-up/page.tsx
+++ ui/src/app/auth/sign-up/page.tsx
@@ -1,14 +1,17 @@
 'use client';
 import { SignUpForm } from "@/app/components/Sign-up";
 import { useAuth } from "@/app/hooks/useAuth";
+import { useAuthContext } from "@/app/hooks/useAuthContext";
 import apiService from "@/app/services/api.service";
+import { redirect } from "next/navigation";
 
 
 const SignUp = () => {
+	const { authState } = useAuthContext();
   const { handleSignUp, initialValuesSignUp } = useAuth();
 
 
-	return (
+	return authState.isAuth ? redirect('/') : (
     <SignUpForm
       apiService={apiService} 
       handleSignUp={handleSignUp} 
