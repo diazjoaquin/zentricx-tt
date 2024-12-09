1adbdd8c5756a655918b2c8dfef19cfb88e76452
Joaquín Díaz <diaz.joaquin.e@gmail.com>
2024-12-09 03:52:00 -0300

    Add options to formField

--- ui/src/app/common/components/FormField.tsx
+++ ui/src/app/common/components/FormField.tsx
@@ -8,6 +8,7 @@ type FormFieldProps = {
   id: string;
   placeHolder: string;
   required: boolean;
+  options?: { value: string; label: string }[];
 }
 
 export const FormField: React.FC<FormFieldProps> = ({
@@ -16,13 +17,23 @@ export const FormField: React.FC<FormFieldProps> = ({
   label,
   id,
   placeHolder,
-  required
+  required,
+  options
 }) => { 
   return (
     <div>
       <label className="block text-sm font-medium leading-6 text-gray-900 px-2" htmlFor={id}>
         {label}
       </label>
+      { type === 'select' ? (
+        <Field as="select" name={name} id={id} required={required} className="block w-full px-2 rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+          {options?.map((option) => (
+            <option className="px-2 py-1" key={option.value} value={option.value}>
+              {option.label}
+            </option>
+        ))}
+      </Field> 
+      ) : (
       <Field
         type={type}
         name={name}
@@ -30,7 +41,7 @@ export const FormField: React.FC<FormFieldProps> = ({
         placeholder={placeHolder}
         required={required}
         className="block w-full px-2 rounded-md border-0 py-1 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-      />
+      />)}
       <div className="min-h-[15px]">
         <ErrorMessage 
           name={name}
