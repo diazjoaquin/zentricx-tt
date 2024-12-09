fcf2547907a6aa4f4c891a4b4ab984684a3641e2
Joaquín Díaz <diaz.joaquin.e@gmail.com>
2024-12-09 03:53:59 -0300

    Update useTasks custom hook

--- ui/src/app/hooks/useTasks.ts
+++ ui/src/app/hooks/useTasks.ts
@@ -1,26 +1,132 @@
+import { UUID } from "crypto";
 import { ITask } from "../interfaces/ITask";
 import apiService from "../services/api.service";
-import { getUserDataByCookie } from "../utils/cookies/cookies.utils";
+import { State } from "../utils/state.enum";
+import { useEffect, useState } from "react";
+import { useNotification } from "./useNotification";
 
-export const useTask = () => {
+export const useTask = (userId: UUID) => {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+  const [tasks, setTasks] = useState<ITask[]>([]);
+  const [ currentTask, setCurrentTask ] = useState<ITask | null>(null);
+  const { getNotification } = useNotification();
 
-  const fetchTasks = async () => {
+  const addInitialValues: ITask = {
+    createdBy: userId,
+    name: "",
+    description: "",
+    state: State.PENDING,
+  };
+
+  const updateInitialValues: ITask = {
+    createdBy: userId,
+    name: currentTask?.name || "",
+    description: currentTask?.description || "",
+    state: currentTask?.state || State.PENDING,
+  };
+  
+
+
+  const handleAddTask = async (values: ITask, onSuccess: () => void) => {
+    setLoading(true);
     try {
-      const payload = await getUserDataByCookie('access_token');
-      console.log(payload);
-      
-      const response: ITask[] = await apiService.get(`task/user/${payload?.id}`);
-      if (response) {
-        console.log(response);
-        return response;
+      const reponse: ITask = await apiService.post('task', values);
+      if (reponse) {
+        setTasks([...tasks, reponse]);
+        onSuccess();
+        getNotification('success', 'Task created successfully');
+        setLoading(false);
       }
     } catch (error) {
-     throw new Error("Error fetching tasks, please try again later."); 
+      if (error instanceof Error) {
+        setError(error.message);
+        getNotification('error', error.message);
+      }
+    } finally {
+      setLoading(false);
     }
-    return [];
+  };
+
+  const handleUpdateTask = async (id: UUID, values: ITask, onSuccess: () => void) => {
+    setLoading(true);
+    try {
+      const response: ITask = await apiService.put(`task/${id}`, values);
+      if (response) {
+        setTasks((prevTasks) =>
+          prevTasks.map((task) =>
+            task.id === id ? response : task
+          )
+        );
+        onSuccess();
+        getNotification('success', 'Task updated successfully');
+        setLoading(false);
+        console.log(response);
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error.message);
+        getNotification('error', error.message);
+      }
+    } finally {
+      setLoading(false);
+    }
   }
 
+  const handleDeleteTask = async (taskId: UUID, onSuccess: () => void) => {
+    setLoading(true);
+    try {
+      const response: boolean = await apiService.delete(`task/${taskId}`);
+      if (response) {
+        setTasks(tasks.filter((task) => task.id !== taskId));
+        onSuccess();
+        getNotification('success', 'Task deleted successfully');
+        setLoading(false);
+        console.log(response);
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error.message);
+        getNotification('error', error.message);
+      }
+    } finally {
+      setLoading(false);
+    }
+  }
+
+
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      setLoading(true);
+      try {
+        const response: ITask[] = await apiService.get(`task/user/${userId}`);
+        if (response) {
+          setTasks(response);
+        }
+        setLoading(false);
+      } catch (error) {
+        if (error instanceof Error) {
+          setError(error.message);
+          getNotification('error', error.message);
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchTasks();
+  }, []);
+
   return {
-    fetchTasks
+    addInitialValues,
+    updateInitialValues,
+    loading,
+    error,
+    tasks,
+    handleAddTask,
+    handleDeleteTask,
+    handleUpdateTask,
+    setCurrentTask
   }
 }
