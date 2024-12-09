55ef8618e301f11ab6728dd7785e9c4be1dd511d
Joaquín Díaz <diaz.joaquin.e@gmail.com>
2024-12-09 03:52:25 -0300

    Update navbar

--- ui/src/app/components/Navbar.tsx
+++ ui/src/app/components/Navbar.tsx
@@ -1,15 +1,15 @@
 'use client';
 import { Button } from "../common/components/Button";
-import { useAuth } from "../hooks/useAuth";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 
 export const Navbar = () => {
-	const { handleLogout } = useAuth();
+	const { handleLogOut } = useAuthContext();
 
   return (
-    <div className="h-16 w-screen flex flex-row justify-end items-center">
+    <div className="h-16 w-screen fixed top-0 border-b-2 bg-[#1f1f1f] flex flex-row justify-end items-center">
       <div className="px-8">
-        <Button label="Logout" type="button" onClick={handleLogout}/>
+        <Button label="Logout" type="button" onClick={handleLogOut}/>
       </div>
     </div>
   )
