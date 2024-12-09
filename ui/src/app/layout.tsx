b3da01c447dd6cfb87acc499b6f4893a0c9aa8c2
Joaquín Díaz <diaz.joaquin.e@gmail.com>
2024-12-09 03:50:59 -0300

    Update layout

--- ui/src/app/layout.tsx
+++ ui/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { AuthProvider } from "./context/auth.context";
+import { NotificationProvider } from "./context/notification.context";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,7 +31,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <AuthProvider>
-          {children}
+          <NotificationProvider>
+            {children}
+          </NotificationProvider>
         </AuthProvider>
       </body>
     </html>
